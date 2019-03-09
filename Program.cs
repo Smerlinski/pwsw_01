d5c7afea3153a137db6ec60c45a4682d694ed8e6
Szymon Smerliński <smerlinski.szymon@gmail.com>
2019-03-09 15:42:49 +0100

    test

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pwsw_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> unikalne = new List<int>();
+            Console.WriteLine("Obliczanie unikalnych cyfr.");
+            Console.WriteLine("Podaj liczbe cyfr, ktore zostana sprawdzone:");
+            int ilosc = Convert.ToInt32(Console.ReadLine());
+            while (ilosc > 0){
+                int cyfra = Convert.ToInt32(Console.ReadLine());
+                if (!unikalne.Contains(cyfra))
+                    unikalne.Add(cyfra);
+                ilosc--;                
+            }
+            Console.WriteLine("Ilosc unikatowych cyft to " + unikalne.Count );
+            Console.ReadKey();
+        }
+    }
+}
